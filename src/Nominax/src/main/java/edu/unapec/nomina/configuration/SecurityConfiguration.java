0ba418d89e8070a6f24119c1c1bc002da1af6465
Victor D. Montero <victordanielmontero@gmail.com>
2017-07-14 00:22:18 -0400

    Tratando de implementar spring security + hibernate (10%)

--- src/Nominax/src/main/java/edu/unapec/nomina/configuration/SecurityConfiguration.java
+++ src/Nominax/src/main/java/edu/unapec/nomina/configuration/SecurityConfiguration.java
@@ -6,11 +6,21 @@
 package edu.unapec.nomina.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 /**
  *
@@ -19,22 +29,61 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    
+
+    private final int TOKEN_VALIDATION_SECONDS = 86400;
+    private final String REMEMBER_ME_NAME = "remember-me";
+
+    @Autowired
+    @Qualifier("customUserDetailsService")
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    PersistentTokenRepository persistentTokenRepository;
+
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("mortal").password("humano").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider());
     }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/home").access("hasRole('USER')")
+                .antMatchers("/", "/home").access("hasRole('USER') or hasRole('ADMIN')")
                 .antMatchers("/empleados/**").access("hasRole('ADMIN')")
                 .and().formLogin().loginPage("/signin")
+                .loginProcessingUrl("/signin")
                 .usernameParameter("username").passwordParameter("password")
+                .and().rememberMe().rememberMeParameter(REMEMBER_ME_NAME)
+                .tokenRepository(persistentTokenRepository)
+                .tokenValiditySeconds(TOKEN_VALIDATION_SECONDS)
                 .and().csrf()
                 .and().exceptionHandling().accessDeniedPage("/denied");
     }
-    
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public PersistentTokenBasedRememberMeServices getPersistentTokenBasedRememberMeServices() {
+        PersistentTokenBasedRememberMeServices tokenBasedServices
+                = new PersistentTokenBasedRememberMeServices(REMEMBER_ME_NAME, userDetailsService, persistentTokenRepository);
+        return tokenBasedServices;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(getPasswordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationTrustResolver getAuthenticationTrustResolver() {
+        return new AuthenticationTrustResolverImpl();
+    }
 }
---
+++ src/Nominax/src/main/java/edu/unapec/nomina/dao/AbstractDao.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+    Tutorial de : http://websystique.com/springmvc/spring-mvc-4-and-spring-security-4-integration-example/
+ */
+package edu.unapec.nomina.dao;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author phenom
+ */
+public abstract class AbstractDao<PK extends Serializable, T> {
+
+    private final Class<T> persistanClass;
+
+    @SuppressWarnings("unchecked")
+    public AbstractDao() {
+        this.persistanClass
+                = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+    }
+
+    @Autowired
+    public SessionFactory sessionFactory;
+
+    protected Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @SuppressWarnings("unchecked")
+    public T getByKey(PK key) {
+        return (T) getSession().get(persistanClass, key);
+    }
+
+    public void persist(T entity) {
+        getSession().persist(entity);
+    }
+
+    public void update(T entity) {
+        getSession().update(entity);
+    }
+
+    public void delete(T entity) {
+        getSession().delete(entity);
+    }
+
+    protected Criteria createEntityCriteria() {
+        return getSession().createCriteria(persistentClass);
+    }
+}
